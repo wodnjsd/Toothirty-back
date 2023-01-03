8b0ff6e9f78f9d720339bdd1af6ebd84746f4476
Jaewon Han <wodnjsd@gmail.com>
2023-01-03 12:18:50 +0000

    back

--- config/environment.py
+++ config/environment.py
@@ -1,4 +1,4 @@
 import os
 
-db_URI = os.getenv('DATABASE_URL', 'postgresql://localhost:5737/teeth_db')
+db_URI = os.getenv('DATABASE_URL', 'postgresql://localhost:5432/teeth_db')
 secret = os.getenv('SECRET', 'Hereisfantasticsecret123')
